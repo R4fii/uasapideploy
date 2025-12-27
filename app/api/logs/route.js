47c0a81fd0b0c01efa5ed8317f1f4581100224fb
ACER ASPIRE <yhsgilbertyhs@gmail.com>
2025-12-27 13:21:39 +0700

    fix bisa2

--- app/api/logs/route.js
+++ app/api/logs/route.js
@@ -16,7 +16,7 @@ export async function POST(request) {
       data: {
         method: body.method,
         pathname: body.pathname,
-        userId: body.id,
+        UserId: body.id,
         role: body.role,
       },
     });
